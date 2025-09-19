ac3195e12c1aba560ee4ef98ca2b048e2f274562
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-19 07:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const next=document.getElementById("next");
+const prev=document.getElementById("prev");
+const progressLineFill=document.querySelector(".progress-line-fill;
+let circleActiveCount=1;											  
+next.addEventListener("click",(e)=>{
+	circleActiveCount+=1;
+	prev.removeAttribute("disabled");
+	let currentWidth=progressLineFill.style.width || "0%";
+	currentWidth=convertToNumber(currentWidth);
+	// let width=currentWidth+25+"%";
+	progressLineFill.style.width=progressLineFill+25+"%";
+	if(width=="100%"){
+		next.disabled="true";
+	}
+	
+});
+function convertToNumber(numInString){
+	// console.log(numInString);
+	numInString=numInString.split("");
+	numInString.pop();
+	numInString=numInString.join("");
+	return Number(numInString);
+}
